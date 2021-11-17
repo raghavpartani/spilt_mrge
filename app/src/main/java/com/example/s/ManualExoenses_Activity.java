8303e9c6a48129d37bc92a4d439ba0cdff791512
Your Name <raghavpartani1@gmail.com>
2021-11-17 09:41:21 +0530

    up

---
+++ app/src/main/java/com/example/s/ManualExoenses_Activity.java
@@ -0,0 +1,14 @@
+package com.example.s;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualExoenses_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_exoenses_);
+    }
+}
